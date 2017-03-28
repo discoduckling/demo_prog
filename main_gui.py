034032bd25909806cde1b1d5760086f562f57523
discoduckling <mndesai@bu.edu>
2017-03-27 22:07:36 -0600

    Added the background color for tone plots and pushbuttons now play tones

--- main_gui.py
+++ main_gui.py
@@ -12,12 +12,15 @@ from matplotlib import style
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
+from winsound import *
 
 # Create Instance of tkinter
 win = tk.Tk()
 win.title("MMI Demo Stand")
 # win.resizable(width=False, height=False)
-win.geometry('{}x{}'.format(1800,1200))
+w, h = win.winfo_screenwidth(), win.winfo_screenheight()
+win.geometry("%dx%d+0+0" % (w-100, h-100))
+
 
 #=============================================
 # Menu Callbacks
@@ -128,8 +131,30 @@ for child in tab1.winfo_children():
     child.grid_configure(padx=20, pady=10, sticky="WE")
 
 #=============================================
+# CREATE CALLBACKS FOR BUTTONS IN TEST TONE DEMO
+#=============================================
+def _play440():
+    "Plays 440Hz tone for 5s"
+    old_color = 'whitesmoke'
+    new_color = 'dodgerblue'
+    # f1.set_facecolor(new_color)
+    PlaySound('440_sound.wav', SND_FILENAME)
+    # f1.set_facecolor(old_color)
+
+def _play350():
+    "Plays 350Hz tone for 5s"
+    PlaySound('350_sound.wav', SND_FILENAME)
+
+def _playDual():
+    "Plays Dual Tone for 5s"
+    PlaySound('dual_sound.wav', SND_FILENAME)
+
+
+#=============================================
 # CREATE OBJECTS IN TAB2: TEST TONE DEMO
 #=============================================
+# Set background color of tab to white
+
 # Create data for tone plots
 pitch1 = 350
 pitch2 = 440
@@ -141,33 +166,56 @@ wav2 = np.sin(2*x_range1*pitch2*math.pi) # original 440 hz
 wav3 = np.sin(2*x_range2*pitch1*math.pi) # zoomed in 350 hz
 wav4 = np.sin(2*x_range2*pitch2*math.pi) # zoomed in 440 hz
 
-t350 = Figure(figsize=(22,15))
-t350.subplots_adjust(right=0.7, left=0.1, top=0.9, bottom=0.1, hspace=0.5)
-a1 = t350.add_subplot(611)
-a2 = t350.add_subplot(612)
-a3 = t350.add_subplot(613)
-a4 = t350.add_subplot(614)
-a5 = t350.add_subplot(615)
-a6 = t350.add_subplot(616)
+f1 = Figure(figsize=(w/110, h/275), facecolor="whitesmoke")
+f2 = Figure(figsize=(w/110, h/275), facecolor="whitesmoke")
+f3 = Figure(figsize=(w/110, h/275), facecolor="whitesmoke")
+
+a1 = f1.add_subplot(211)
+a2 = f1.add_subplot(212)
+a3 = f2.add_subplot(211)
+a4 = f2.add_subplot(212)
+a5 = f3.add_subplot(211)
+a6 = f3.add_subplot(212)
 
 a1.plot(x_range1, wav1)
-a1.set_ylabel("First Tone 350 Hz")
+a1.set_title("First Tone 350 Hz")
 a2.plot(x_range2, wav3)
-a2.set_ylabel("Zoomed...")
+a2.set_title("Zoomed...")
 a3.plot(x_range1, wav2)
-a3.set_ylabel("Second Tone 440 Hz")
+a3.set_title("Second Tone 440 Hz")
 a4.plot(x_range2, wav4)
-a4.set_ylabel("Zoomed...")
+a4.set_title("Zoomed...")
 a5.plot(x_range1, (wav1 + wav2))
-a5.set_ylabel("Combined 350 & \n440 Hz Tones")
+a5.set_title("Combined 350 & 440 Hz Tones")
 a6.plot(x_range2, (wav3 + wav4))
-a6.set_ylabel("Zoomed")
+a6.set_title("Zoomed")
 
-canvasT350 = FigureCanvasTkAgg(t350, tab2)
+# plt.setp([a.get_xticklabels() for a in f1.axes[:-1]], visible=False)
+plt.setp([a.get_yticklabels() for a in f1.axes], visible=False)
+# plt.setp([a.get_xticklabels() for a in f2.axes[:-1]], visible=False)
+plt.setp([a.get_yticklabels() for a in f2.axes], visible=False)
+# plt.setp([a.get_xticklabels() for a in f3.axes[:-1]], visible=False)
+plt.setp([a.get_yticklabels() for a in f3.axes], visible=False)
 
-canvasT350.show()
+canvasf1 = FigureCanvasTkAgg(f1, tab2)
+canvasf2 = FigureCanvasTkAgg(f2, tab2)
+canvasf3 = FigureCanvasTkAgg(f3, tab2)
+canvasf1.show()
+canvasf2.show()
+canvasf3.show()
+canvasf1.get_tk_widget().grid(column=1, row=0, sticky="W")
+canvasf2.get_tk_widget().grid(column=1, row=1, sticky="W")
+canvasf3.get_tk_widget().grid(column=1, row=2, sticky="W")
+f1.tight_layout()
+f2.tight_layout()
+f3.tight_layout()
 
-canvasT350.get_tk_widget().grid(column=0, row=0, pady=15)
+playButton1 = ttk.Button(tab2, text="Play 350 Hz", command=_play350)
+playButton1.grid(column=0, row=0, padx=10)
+playButton2 = ttk.Button(tab2, text="Play 440 Hz", command=_play440)
+playButton2.grid(column=0, row=1, padx=10)
+playButton3 = ttk.Button(tab2, text="Play Dual Tone", command=_playDual)
+playButton3.grid(column=0, row=2, padx=10)
 
 
 #=============================================
--- making_noise_in_python.py
+++ making_noise_in_python.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     # stream2 = p.open(format=pyaudio.paFloat32, channels=1, rate=44100, output=1)
     play_tone(stream1)
     play_tone(stream1, frequency=440)
-    play_dial_tone(stream1, 350, 440)
+    # play_dial_tone(stream1, 350, 440)
     # play_tone(stream2, frequency=440)
     stream1.close()
     stream2.close()
